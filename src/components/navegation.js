e752b668aa3226278d8fbe100339e6df6e5e96f6
loustadler <loustadler@hotmail.com>
2020-02-15 23:46:21 +0000

    No start bootstrap

--- src/components/navegation.js
+++ src/components/navegation.js
@@ -5,11 +5,6 @@ import React from "react"
 const Navegation = ({ bgColor }) => {
     return <nav className={`navbar ${bgColor} navbar-expand-lg navbar-light fixed-top`} id="mainNav">
         <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i className="fas fa-bars"></i>
-            </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
