7fa93618184a4fd46a19694b27fab2b9b5d1180a
loustadler <loustadler@hotmail.com>
2020-02-15 23:48:05 +0000

    No header

--- src/components/navegation.js
+++ src/components/navegation.js
@@ -5,19 +5,6 @@ import React from "react"
 const Navegation = ({ bgColor }) => {
     return <nav className={`navbar ${bgColor} navbar-expand-lg navbar-light fixed-top`} id="mainNav">
         <div className="container">
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#download">Download</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#features">Features</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                </li>
-                </ul>
-            </div>
         </div>
     </nav>;
 }
