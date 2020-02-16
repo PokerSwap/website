6715b2fe547b911086d0a671db315b81c1452d9a
loustadler <loustadler@hotmail.com>
2020-02-16 04:32:35 +0000

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "../components/navegation"
 import "../styles/main.css"
-import demoScreenURL from "../images/demo-screen-1.jpg";
+import demoScreenURL from "../images/LoginScreenShot.png";
 import googlePlayBadge from "../images/google-play-badge.svg";
 import appStoreBadge from "../images/app-store-badge.svg";
+import twitterURL from "../images/twitter.svg";
+import facebookURL from "../images/facebook (1).svg";
+import instagramURL from "../images/instagram.svg";
+import { Link } from "gatsby";
+import crossfitpic from "../images/Crossfit picture.jpg";
+import fishingpic from "../images/Fishing picture.jpg";
+import golfpic from "../images/Golf picture.jpg";
+import pokerpic from "../images/Poker picture.jpg"
 
 const Home = () => {
     return <>
@@ -12,9 +20,13 @@ const Home = () => {
             <div className="container h-100">
             <div className="row h-100">
                 <div className="col-lg-7 my-auto">
-                <div className="header-content mx-auto text-center">
-                    <h1 className="mb-5">Welcome to Swap Profit! <br></br><br></br>Your new exchange model for competitive gaming amongst peers where you can Swap potential Profits and earn rewards in your events. </h1>
-                    <a href="#contact" className="btn btn-outline btn-xl js-scroll-trigger">Let's Start Swapping!</a>
+                <div className="header-content text-center">
+                    <h1 className="mb-5">Welcome to Swap Profit! </h1>
+                    <p>
+                        Your new exchange model for competitive gaming amongst peers where you can Swap 
+                        potential Profits and earn rewards in your events. 
+                    </p>
+                    <a href="#buttons" className="btn btn-outline btn-xl js-scroll-trigger">Let's Start Swapping!</a>
                 </div>
                 </div>
                 <div className="col-lg-5 my-auto">
@@ -34,13 +46,13 @@ const Home = () => {
             </div>
         </header>
         <section className="download bg-primary text-center" id="download">
-            <div className="container">
+            <div id="buttons" className="container">
                 <div className="row">
                     <div className="col-md-8 mx-auto">
                         <h2 className="section-heading">Download the app to get started!</h2>
                         <div className="badges">
                             <a className="badge-link" href="#"><img src={googlePlayBadge} alt="" /></a>
-                            <a className="badge-link" href="#"><img src={appStoreBadge} alt="" /></a>
+                            <a className="badge-link ml-3" href="#"><img src={appStoreBadge} alt="" /></a>
                         </div>
                     </div>
                 </div>
@@ -74,15 +86,19 @@ const Home = () => {
                     <div className="col-lg-6">
                         <div className="feature-item">
                         <i className="icon-screen-smartphone text-primary"></i>
-                        <h3>Fishing Tournaments</h3>
-                        <p className="text-muted">Coming soon!</p>
+                        <div className="screen">
+                        <img src={fishingpic} className="img-fluid" alt="" />
+                        </div>
+                        <p className="text-muted">Fishing Tournaments</p>
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="feature-item">
                         <i className="icon-camera text-primary"></i>
-                        <h3>Golf Tournaments</h3>
-                        <p className="text-muted">Coming soon!</p>
+                        <div className="screen">
+                        <img src={golfpic} className="img-fluid" alt="" />
+                        </div>
+                        <p className="text-muted">Golf Tournaments</p>
                         </div>
                     </div>
                     </div>
@@ -90,15 +106,19 @@ const Home = () => {
                     <div className="col-lg-6">
                         <div className="feature-item">
                         <i className="icon-present text-primary"></i>
-                        <h3>Poker Tournaments</h3>
-                        <p className="text-muted">Coming soon!</p>
+                        <div className="screen">
+                        <img src={pokerpic} className="img-fluid" alt="" />
+                        </div>
+                        <p className="text-muted">Poker Tournaments</p>
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="feature-item">
                         <i className="icon-lock-open text-primary"></i>
-                        <h3>Fitness Competitions</h3>
-                        <p className="text-muted">Coming soon!</p>
+                        <div className="screen">
+                        <img src={crossfitpic} className="img-fluid" alt="" />
+                        </div>
+                        <p className="text-muted">Fitness Competitions</p>
                         </div>
                     </div>
                     </div>
@@ -112,7 +132,7 @@ const Home = () => {
             <div className="cta-content">
             <div className="container">
                 <h2>Done reading?<br />Start Swapping!</h2>
-                <a href="#contact" className="btn btn-outline btn-xl js-scroll-trigger">Let's Start Swapping!</a>
+                <a href="#buttons" className="btn btn-outline btn-xl js-scroll-trigger">Let's Start Swapping!</a>
             </div>
             </div>
             <div className="overlay"></div>
@@ -124,17 +144,17 @@ const Home = () => {
             <ul className="list-inline list-social">
                 <li className="list-inline-item social-twitter">
                 <a href="#">
-                    <i className="fab fa-twitter"></i>
+                    <img src={twitterURL} className="icon" />
                 </a>
                 </li>
                 <li className="list-inline-item social-facebook">
                 <a href="#">
-                    <i className="fab fa-facebook-f"></i>
+                    <img src={facebookURL} className="icon" />
                 </a>
                 </li>
-                <li className="list-inline-item social-google-plus">
+                <li className="list-inline-item social-instagram">
                 <a href="#">
-                    <i className="fab fa-google-plus-g"></i>
+                    <img src={instagramURL} className="icon" />
                 </a>
                 </li>
             </ul>
@@ -143,16 +163,22 @@ const Home = () => {
 
         <footer>
             <div className="container">
-            <p>&copy; Your Website 2019. All Rights Reserved.</p>
+            <p>&copy; Swap Profit, LLC   2020. All Rights Reserved.</p>
             <ul className="list-inline">
                 <li className="list-inline-item">
-                <a href="#">Privacy</a>
+                <Link to="/">Home</Link> 
+                <li className="list-inline-item">
+                </li>
+                <Link to="/privacy">Privacy Policy</Link>
                 </li>
                 <li className="list-inline-item">
-                <a href="#">Terms</a>
+                <Link to="/terms">Terms and Conditions</Link>
                 </li>
                 <li className="list-inline-item">
-                <a href="#">FAQ</a>
+                <Link to="/faqs">FAQ's</Link>
+                </li>
+                <li className="list-inline-item">
+                <a href="mailto:info@swapprofitonline.com">Contact</a>
                 </li>
             </ul>
             </div>
