6236052ce58916f2f99a1e6924ff264b89cb8ef7
loustadler <loustadler@hotmail.com>
2020-03-02 23:59:32 +0000

    h1

--- src/pages/faqspoker.js
+++ src/pages/faqspoker.js
@@ -11,13 +11,28 @@ const Home = () => {
         <Header bgColor="bg-dark" />
         <section className="contact" id="contact">
             <div className="container text-left">
-            <h1><strong>SWAP FAQ’s</strong></h1>
+            <h1><strong>SWAP FAQ’s - Poker Tournaments</strong></h1>
 <p><strong>&nbsp;</strong></p>
 <p><strong>How does Swap Profit work?</strong></p>
-<p>Swap Profit provides the ability for competitors in events to Swap percentages of potential profits with each other, as well as earn rewards for event entries. &nbsp; Swap Profit provides event information, ability to Swap within events, and event results.</p>
+<p>Swap Profit provides the ability for competitors in events to Swap percentages of potential profits with each other, as well as earn rewards for event entries. &nbsp;Swap Profit provides event information, ability to Swap within events, and event results.</p>
 <p>&nbsp;</p>
 <p><strong>Does it cost me anything to Swap?</strong></p>
 <p>All Swaps require Swap Tokens – Swap Token amounts may vary based on the event or the type of Swap.</p>
+<p><strong>For Poker Tournaments:</strong></p>
+<p><strong>Swaps for single-day events or Day 1 on Multi-Day events with Buy-In amounts between:</strong></p>
+<p>$0 - $499 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;= 1 Swap Token per Swap</p>
+<p>$500 - $999 &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;= 2 Swap Tokens per Swap</p>
+<p>$1,000+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;= 3 Swap Tokens per Swap</p>
+<p><strong>Additional Tokens for Swaps in Multi-Day events:</strong></p>
+<p>Day 2 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +1 Additional Swap Token</p>
+<p>Day 3 Swaps&nbsp; &nbsp; &nbsp; &nbsp;= +2 Additional Swap Tokens</p>
+<p>Day 4 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +3 Additional Swap Tokens</p>
+<p>Day 5 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +4 Additional Swap Tokens</p>
+<p>Day 6 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +5 Additional Swap Tokens</p>
+<p>Day 7 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +6 Additional Swap Tokens</p>
+<p>Day 8 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +7 Additional Swap Tokens</p>
+<p>Day 9 Swaps &nbsp; &nbsp; &nbsp; &nbsp;= +8 Additional Swap Tokens</p>
+<p>Day 10+ Swaps&nbsp; &nbsp;= +9 Additional Swap Tokens</p>
 <p>&nbsp;</p>
 <p><strong>Who can see who I am Swapping with?</strong></p>
 <p>Only you and your Swappers can see your Swaps.</p>
@@ -29,8 +44,8 @@ const Home = () => {
 <p>The Swap Profit Model is:</p>
 <p>Your Earnings in the Event <strong>MINUS ONE (1) entry fee</strong> in your event = Profit that counts towards your Swap.</p>
 <p>&nbsp;</p>
-<p><strong>What happens if I am in a buy-in event with re-entries and I re-enter the event? &nbsp; Are all of my Swaps cancelled from my first entry buy-in?</strong></p>
-<p>Once you agree to a Swap in an event, <strong>any and all Swaps in that event remain active for the duration of the event regardless of how many times you re-enter</strong>. &nbsp; No, your Swaps from your first entry buy-in are NOT cancelled – they will remain active for the duration of the event.</p>
+<p><strong>What happens if I am in a buy-in event with re-entries and I re-enter the event? &nbsp;Are all of my Swaps cancelled from my first entry buy-in?</strong></p>
+<p>Once you agree to a Swap in an event, <strong>any and all Swaps in that event remain active for the duration of the event regardless of how many times you re-enter</strong>. &nbsp;No, your Swaps from your first entry buy-in are NOT cancelled – they will remain active for the duration of the event.</p>
 <p><strong>&nbsp;</strong></p>
 <p><strong>How do I collect my Swap earnings from the event?</strong></p>
 <p><strong>Swap Profit DOES NOT have any involvement in the procuring of earnings for Swaps between Users</strong>. &nbsp;Once you have received your Swap Results Email, it is purely up to you to collect/pay your Swaps. &nbsp;You will have a temporary chat available through the Swap App with any Users where a Swap Result has qualified of earnings. &nbsp;Please keep in mind that your Swap Rating will be affected by promptness of your Swap transaction finality.</p>
@@ -49,36 +64,51 @@ const Home = () => {
 <p><strong>ONE (1) STAR RATING</strong>: &nbsp;If you have not paid your Swap after 168 hours (7 calendar days) from receipt of Swap Results, you will receive a One-Star Rating for that Swap; your Swap account will be frozen until that Swap has been paid out; and you will be on the “Naughty List.”</p>
 <p>&nbsp;</p>
 <p><strong>What happens if I don’t pay out on a Swap?</strong></p>
-<p>If you do not pay out on a Swap after seven (7) calendar days from receipt of Swap Results, your Swap Account will be frozen and you will not be able to continue to Swap until your pending Swaps have been finalized to completion. &nbsp; Please be aware that your original Swap Agreement – emailed to both Swap parties – is legally binding. &nbsp;You may end up in court if your Swapper goes that route. &nbsp;You will also be put on the “Naughty List.”</p>
+<p>If you do not pay out on a Swap after seven (7) calendar days from receipt of Swap Results, your Swap Account will be frozen and you will not be able to continue to Swap until your pending Swaps have been finalized to completion. &nbsp;Please be aware that your original Swap Agreement – emailed to both Swap parties – is legally binding. &nbsp;You may end up in court if your Swapper goes that route. &nbsp;You will also be put on the “Naughty List.”</p>
 <p>&nbsp;</p>
 <p><strong>What is the “Naughty List”?</strong></p>
 <p>The Naughty List is the public listing of Users who have open unpaid One-Star Swaps (they have not paid their Swaps after seven calendar days of receiving Swap Results). &nbsp;If you are hoping to collect on potential earnings from your Swappers, it may be to your benefit to not Swap with these Users.</p>
 <p>&nbsp;</p>
 <p><strong>How do I get removed from the “Naughty List”?</strong></p>
 <p>Pay out your unpaid Swap(s).</p>
-<p>&nbsp;</p>
+<p><strong>&nbsp;</strong></p>
 <p><strong>I am trying to pay out on a Swap, but I am having issues. &nbsp;What can I do so that my Swap Rating does not get affected by this?</strong></p>
 <p>If you are having trouble paying out on a Swap, click on the “Swap Issues” button in that Swap and email our Swap Support Team. &nbsp;We will help you resolve the issue and adjust your Swap Rating accordingly.</p>
 <p>&nbsp;</p>
 <p><strong>What about taxes? &nbsp;Are my Swap payouts and/or earnings based on tax payouts?</strong></p>
 <p>No, all Swap payouts and/or earnings are not based on tax payouts. &nbsp;Swap payouts and earnings are NOT based on any potential tax implications. &nbsp;All Swap Users are liable for their own accounting for taxes SEPARATE of any Swap payouts and/or earnings.</p>
-<p>&nbsp;</p>
+<p><strong>&nbsp;</strong></p>
 <p><strong>Can I do additional Swaps in the same event with the same Users I already have active Swaps with?</strong></p>
 <p>Yes. &nbsp; You can do as many Swaps with whomever you would like – including Users you already have active Swaps with - as long as you have not gone over your 50% Swap threshold in that individual event.</p>
 <p>&nbsp;</p>
 <p><strong>I am trying to do another Swap but my Swap percentage for the event is showing “Maxed Out.” &nbsp;What do I do?</strong></p>
 <p>All Users are allowed to do a maximum of fifty (50) percent of Swaps in any single event. &nbsp;If you are receiving the “Maxed Out” alert in an event, that means that you have already reached your 50% maximum for that particular event. &nbsp;If you have any “Pending Swaps” in that event, you may decide to cancel them to Swap with another User.</p>
-<p>&nbsp;</p>
+<p><strong>&nbsp;</strong></p>
 <p><strong>The event that I was in has concluded. &nbsp;When will I receive the official results from the event?</strong></p>
 <p>As soon as we receive the official results from any event, we will send out Swap Results for that event.</p>
+<p><strong>&nbsp;</strong></p>
+<p><strong>Someone else has claimed my Hendon Mob profile. &nbsp; What do I do?</strong></p>
+<p>Contact our Swap Support Team – we will help you resolve this issue.</p>
 <p>&nbsp;</p>
-<p><strong>I won an additional prize separate from cash payouts in my event. &nbsp;Does this additional prize count towards Swap earnings?</strong></p>
-<p>No, additional prize(s) that are won in any event will NOT count as earnings towards Swaps. &nbsp; Only your event cash payout earnings will count towards Swaps.</p>
+<p><strong>I chopped my event, but the Official Results show that I was paid a different amount than what I received. &nbsp;What do I do?</strong></p>
+<p>Swap Payouts are based on the Official Results received directly from the event. &nbsp;Before agreeing to a chop, you should ask a Tournament Director at the venue your event is at how the results will be posted. &nbsp;You will be held liable for the earnings amount shown in any Official Results that Swap receives.</p>
+<p>&nbsp;</p>
+<p><strong>I am in a Bounty event. &nbsp;Do Bounties count towards Swap earnings?</strong></p>
+<p>No, Bounties will NOT count as earnings towards Swaps. &nbsp; Only your event cash payout earnings will count towards Swaps.</p>
+<p>&nbsp;</p>
+<p><strong>I won a High Hand in the event I was in. &nbsp;Does High Hand count towards Swap earnings?</strong></p>
+<p>No, High Hand payouts will NOT count as earnings towards Swaps. &nbsp;Only your event cash payout earnings will count towards Swaps.</p>
+<p>&nbsp;</p>
+<p><strong>I won an additional seat entry in the event I was in. &nbsp; Does the additional seat entry count towards Swap earnings?</strong></p>
+<p>No, additional seat entry winnings will NOT count as earnings towards Swaps. &nbsp;Only your event cash payout earnings will count towards Swaps.</p>
+<p>&nbsp;</p>
+<p><strong>I won an additional prize for bagging my amount of chips in an opening flight. &nbsp;Does this additional prize count towards Swap earnings?</strong></p>
+<p>No, additional prize(s) for bagging any amount of chips will NOT count as earnings towards Swaps. &nbsp; Only your event cash payout earnings will count towards Swaps.</p>
 <p>&nbsp;</p>
 <p><strong>I won a trophy in my event. &nbsp;Do I have to share my trophy with my Swappers?</strong></p>
-<p>No, the trophy is yours. &nbsp;Nice job! 😊</p>
-
-
+<p>No, the trophy is yours. &nbsp;Nice job!&nbsp;😊</p>
+<p><strong>&nbsp;</strong></p>
+<p>&nbsp;</p>
             </div>
         </section>
 
